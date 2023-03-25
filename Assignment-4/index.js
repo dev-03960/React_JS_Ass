d88b93af6fae834983f97eebf74c7cc193105023
dev-03960 <dewanshbhargav@gmail.com>
2023-03-25 18:02:50 +0530

    Assignment-4

---
+++ Assignment-4/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+import reactDom from "react-dom/client";
+import logo from './Assets/logo.png';
+
+import icon2 from './Assets/icons8-facebook.svg';
+import icon3 from './Assets/icons8-twitter.svg';
+import icon4 from './Assets/icons8-instagram.svg';
+import './styles/style.css';
+
+
+const header = () =>{
+
+return (
+    <><div className="navbar">
+    {image()}
+    {search()}
+    {icons()}
+ </div>
+ <br></br>
+    </>
+ 
+);
+
+}
+const image = () =>{
+
+    return(
+       <div className="left">
+        <img src={logo} className = "logo" alt="logo" />
+       </div>
+
+    );
+}
+const search = ()=>{
+
+    return(
+           <div className="middle">
+            <button>All</button>
+            <input type="text" name="search" id="search"></input>
+            <button>Search</button>
+           </div>
+    );
+}
+
+const icons = () =>{
+
+    return (
+        <div className="right">
+        
+            
+            <a href="#"><img src={icon2} alt="icon2"/></a>
+            <a href="#"><img src={icon3} alt="icon3"/></a>
+            <a href="#"><img src={icon4} alt="icon4"/></a>
+            
+        
+    </div>
+    )
+}
+
+   
+
+
+const root = reactDom.createRoot(document.getElementById("root"));
+root.render(header());
